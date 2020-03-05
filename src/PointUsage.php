48f33417d0244872573fc9301e794e0f9c70fcba
vayupranaditya <bagusvayuprana@gmail.com>
2020-03-06 00:50:29 +0700

    fixed wrong table name

--- src/PointUsage.php
+++ src/PointUsage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PointUsage extends Model
 {
-    protected $table = 'package_usage';
+    protected $table = 'point_usage';
 
     protected $fillable = [
     	'user_id', 'amount', 'description'
